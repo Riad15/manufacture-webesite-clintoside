1fc4950df6c52a1d6e1f4a94fe7e32280b0179e4
asif riad <asifriad15@gmail.com>
2022-05-23 10:32:43 +0600

    creat react asssignment 12 and add to navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+
+import { Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './Components/Shared/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      {/* <h1 className='text-3xl font-bold'>ComputerS Machinery</h1> */}
+      <Navbar></Navbar>
+      <Routes>
+
+      </Routes>
     </div>
   );
 }
